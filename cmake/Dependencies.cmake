14be47beeb40355cccf2e55acd38d9a8e71b2166
dopedud <theabeed01@gmail.com>
2025-10-05 15:07:31 +0800

    wip

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -46,6 +46,17 @@ set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(glfw)
 # END FETCH GLFW
 
+# FETCH DEAR IMGUI
+set(DEAR_IMGUI_VERSION v1.92.3-docking)
+FetchContent_Declare(
+    dear_imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG ${DEAR_IMGUI_VERSION}
+)
+
+FetchContent_MakeAvailable(dear_imgui)
+# END FETCH DEAR IMGUI
+
 # BUILD GLAD
 # NOTE: GLAD is a special case for dependency management because unlike other dependencies where you have to fetch them
 # online from GitHub or other repositories, GLAD does not have that and instead let developers fetch them via a
@@ -53,3 +64,30 @@ FetchContent_MakeAvailable(glfw)
 # handled manually and should be treated like one (that means no modifying willy-nilly!!!).
 add_subdirectory(${CMAKE_SOURCE_DIR}/src/external/glad)
 # END BUILD GLAD
+
+# BUILD DEAR IMGUI
+set(DEAR_IMGUI_NAME "dear_imgui" CACHE STRING "" FORCE)
+
+add_library(dear_imgui STATIC
+    ${dear_imgui_SOURCE_DIR}/imgui.cpp
+    ${dear_imgui_SOURCE_DIR}/imgui_draw.cpp
+    ${dear_imgui_SOURCE_DIR}/imgui_tables.cpp
+    ${dear_imgui_SOURCE_DIR}/imgui_widgets.cpp
+    ${dear_imgui_SOURCE_DIR}/imgui_demo.cpp
+
+    ${dear_imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+    ${dear_imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+)
+
+target_include_directories(dear_imgui PUBLIC
+    ${dear_imgui_SOURCE_DIR}
+    ${dear_imgui_SOURCE_DIR}/backends
+)
+
+target_link_libraries(dear_imgui PUBLIC glfw glad)
+
+set_target_properties(${GLAD_NAME} PROPERTIES 
+    OUTPUT_NAME "${GLAD_NAME}"
+    PREFIX ""
+)
+# END BUILD DEAR IMGUI
