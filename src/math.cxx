d4556eed9d4ed1e03a4c2047a88583a8b1999b68
Abeed <theabeed01@gmail.com>
2025-11-10 08:07:36 +0800

    wip

---
+++ src/math.cxx
@@ -0,0 +1,17 @@
+#include "JAGE/math.h"
+
+#include "log.h"
+
+#include <glm/glm.hpp>
+
+namespace JAGE
+{
+    namespace Math
+    {
+        void translate()
+        {
+            JAGE_MSG_DEBUG("CREATED A THING.");
+            glm::mat4 model { 1.0f };
+        }
+    }
+}
